843422613b8a76c60e3ab5fd36fb5b891b70b04f
Margaret K. Geiger <mkg@fastmail.com>
2025-04-08 15:33:56 -0700

    add red bullet shooter that fires at player position in salvos

--- shmuptest/scripts/2d/level.gd
+++ shmuptest/scripts/2d/level.gd
@@ -4,21 +4,45 @@ extends Node2D
 
 var _player: Node2D
 var _blue_bullet_shooter: LinearBulletShooter
+var _red_bullet_shooter: LinearBulletShooter
 
 
 func _ready():
 	var blue_bullet = preload("res://scenes/bluebullet.tscn")
-	var shoot_blue_bullet = func(elapsed_sec: float, since_last_shoot_sec: float) -> Array:
+	var shoot_blue_bullet = func(elapsed_sec: float,
+								since_last_shoot_sec: float,
+								salvo_count,
+								target: Node2D) -> Array:
 		var radians_per_shoot = 2.0 * PI / 64
 		var fire_rate = 0.2
 		var speed = 75
 		if since_last_shoot_sec >= fire_rate:
 			var v = Vector2(1, 0)
 			var angle = (elapsed_sec / fire_rate) * radians_per_shoot
-			return [true, v.rotated(angle) * speed]
-		return [false, Vector2()]
+			return [true, v.rotated(angle) * speed, salvo_count]
+		return [false, Vector2(), salvo_count]
 	self._blue_bullet_shooter = LinearBulletShooter.new(blue_bullet,
 														shoot_blue_bullet)
+
+	var red_bullet = preload("res://scenes/redbullet.tscn")
+	var shoot_red_bullet = func(elapsed_sec: float,
+								since_last_shoot_sec: float,
+								salvo_count,
+								target: Node2D) -> Array:
+		var salvo_size = 4
+		var salvo_break = 0.6
+		var fire_rate = 0.3
+		var speed = 150
+		if salvo_count >= salvo_size:
+			if salvo_break >= since_last_shoot_sec:
+				return [false, Vector2(), salvo_count]
+			return [false, Vector2(), 0]
+		if since_last_shoot_sec >= fire_rate:
+			var v = Vector2(1, 0)
+			var angle = self.shooter.get_angle_to(target.global_position)
+			return [true, v.rotated(angle) * speed, salvo_count + 1]
+		return [false, Vector2(), salvo_count]
+	self._red_bullet_shooter = LinearBulletShooter.new(red_bullet, shoot_red_bullet)
 
 	self._player = preload("res://scenes/player.tscn").instantiate()
 	self._player.ui_hits = $hud/hits
@@ -27,6 +51,9 @@ func _ready():
 	self._player.transform.origin = Vector2(250, 400)
 
 func _process(delta_sec: float):
-	var bullet = self._blue_bullet_shooter.maybe_shoot(delta_sec, self._player)
-	if bullet != null:
-		self.shooter.add_child(bullet)
+	var blue_bullet = self._blue_bullet_shooter.maybe_shoot(delta_sec, self._player)
+	var red_bullet = self._red_bullet_shooter.maybe_shoot(delta_sec, self._player)
+	if blue_bullet != null:
+		self.shooter.add_child(blue_bullet)
+	if red_bullet != null:
+		self.shooter.add_child(red_bullet)
--- shmuptest/scripts/linearbulletshooter.gd
+++ shmuptest/scripts/linearbulletshooter.gd
@@ -3,6 +3,7 @@ class_name LinearBulletShooter extends RefCounted
 var _bullet_res: Resource
 var _velocity_from_time: Callable
 
+var _salvo_count
 var _elapsed_sec
 var _since_last_shoot_sec: float
 
@@ -11,22 +12,27 @@ var _since_last_shoot_sec: float
 ##					         since_last_shoot_sec: float) -> Array[bool, Vector2]
 ## The Callable should return true when a bullet should be
 ## made and the bullet should have velocity specified
-## by the second return value. The parameter is the total seconds elapsed
-## and the seconds since the last shoot returned true
+## by the second return value. The parameter is the total seconds elapsed,
+## the seconds since the last shoot returned true,
+## the number of shots fired in the current salvo and the target
 func _init(bullet_res: Resource,
 		   velocity_from_time: Callable):
 	self._bullet_res = bullet_res
 	self._velocity_from_time = velocity_from_time
 	self._elapsed_sec = 0.0
 	self._since_last_shoot_sec = 0.0
+	self._salvo_count = 0
 
 func maybe_shoot(delta_sec: float, target: Node2D) -> Node2D:
 	self._elapsed_sec += delta_sec
 	self._since_last_shoot_sec += delta_sec
 	var ret = self._velocity_from_time.call(self._elapsed_sec,
-											self._since_last_shoot_sec)
+											self._since_last_shoot_sec,
+											self._salvo_count,
+											target)
 	var make_bullet = ret[0]
 	var v = ret[1]
+	self._salvo_count = ret[2]
 	if not make_bullet:
 		return null
 
